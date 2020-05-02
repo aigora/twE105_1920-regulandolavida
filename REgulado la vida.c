067c700d5a3d0a1e5fa14d01da75120641dac99e
andreamartin09 <61228444+andreamartin09@users.noreply.github.com>
2020-05-02 13:59:03 +0200

    principio del trabajo
    he empezado el trabajo designando las variables que voy a utilizar

---
+++ REgulado la vida.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+
+int i,j,k,m,l,cont;				
+int dia,mes,ano,semana,moduloMes,febrero,total_mes;
+int seleccion=1;					
+i=j=k=m=l=cont=0; 				
+
